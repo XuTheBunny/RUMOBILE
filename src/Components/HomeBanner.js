f0565a33b2f1500985c6693cb2408cbfa5c7d4f1
XuTheBunny <xzhu.fg@gmail.com>
2019-04-14 18:32:35 -0400

    ✨ Add Home screen with banner

--- src/Components/HomeBanner.js
+++ src/Components/HomeBanner.js
@@ -1,28 +1,30 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-
-const HomeBanner = (props) => (
+const HomeBanner = props => (
   <View style={styles.view}>
     <Text style={styles.text}>{props.message}</Text>
-    <Image style={{ height: 27, width: 42 }} source={require('../images/bannerBear.png')} />
+    <Image
+      style={{ height: 27, width: 42 }}
+      source={require("../images/bannerBear.png")}
+    />
   </View>
 );
 
 const styles = {
   view: {
     height: 26.9,
-    backgroundColor: '#ed4545',
+    backgroundColor: "#ed4545",
     marginLeft: 12,
     marginRight: 12,
     borderRadius: 5,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
   },
   text: {
     marginLeft: 10,
-    color: 'white'
+    color: "white"
   }
 };
 
