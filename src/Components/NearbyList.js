afbe65b6f9e00899ccff4ecb7e5e6599c3900ffa
XuTheBunny <xzhu.fg@gmail.com>
2019-04-14 18:29:21 -0400

    ✨ Add back Bus screen with Stop and Router

--- src/Components/NearbyList.js
+++ src/Components/NearbyList.js
@@ -1,41 +1,35 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { View, Text, TouchableWithoutFeedback, Image } from 'react-native';
-import { getBusStops } from '../actions';
-import NearbyItem from './NearbyItem';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { View, Text, TouchableWithoutFeedback, Image } from "react-native";
+import { getBusStops } from "../actions";
+import NearbyItem from "./NearbyItem";
 
 class NearbyList extends Component {
-
   renderList() {
-    if(this.props.check == 'here') {
-      return this.props.nearby.map(stop =>
+    if (this.props.check == "here") {
+      return this.props.nearby.map(stop => (
         <NearbyItem stop={stop} key={stop.name} />
-      );
+      ));
     } else {
-      return (
-        <Text>Pulling Data, Please Wait</Text>
-      );
+      return <Text>Pulling Data, Please Wait</Text>;
     }
   }
 
-    render() {
-      return (
-        <View>
-          {this.renderList()}
-        </View>
-      );
-    }
-
+  render() {
+    return <View>{this.renderList()}</View>;
+  }
 }
 
-const styles = {
-};
+const styles = {};
 
 const mapStateToProps = state => {
   return {
-      nearby: state.bus.nb_data,
-      check: state.bus.data_here
+    nearby: state.bus.nb_data,
+    check: state.bus.data_here
   };
 };
 
-export default connect(mapStateToProps, { getBusStops })(NearbyList);
+export default connect(
+  mapStateToProps,
+  { getBusStops }
+)(NearbyList);
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,25 +1,23 @@
-export const FIREBASE_LOGIN = 'firebase_login';
-export const TIME = 'time';
+export const FIREBASE_LOGIN = "firebase_login";
+export const TIME = "time";
 
+export const BANNER_PULL = "banner_pull";
+export const HOME_DATE = "home_date";
 
-export const BANNER_PULL = 'banner_pull';
-export const HOME_DATE = 'home_date';
+export const TRYING_FOOD_PULL = "trying_food_pull";
+export const FOOD_PULL = "food_pull";
 
+export const BROWER_FOOD = "brower_food";
+export const BUSCH_FOOD = "busch_food";
+export const LIVINGSTON_FOOD = "livingston_food";
+export const NEILSON_FOOD = "neilson_food";
 
-export const TRYING_FOOD_PULL = 'trying_food_pull';
-export const FOOD_PULL = 'food_pull';
+export const FOOD_TAB_CHANGED = "food_tab_changed";
 
-export const BROWER_FOOD = 'brower_food';
-export const BUSCH_FOOD = 'busch_food';
-export const LIVINGSTON_FOOD = 'livingston_food';
-export const NEILSON_FOOD = 'neilson_food';
+export const NEARBYBUS = "nearbyBus";
+export const ALLBUS = "allBus";
+export const ACTIVEROUTES = "activeRoutes";
+export const INACTIVEROUTES = "inactiveRoutes";
+export const BUS_DATA_HERE = "bus_data_here";
 
-export const FOOD_TAB_CHANGED = 'food_tab_changed';
-
-export const NEARBYBUS = 'nearbyBus';
-export const ALLBUS = 'allBus';
-export const ACTIVEROUTES = 'activeRoutes';
-export const INACTIVEROUTES = 'inactiveRoutes';
-export const BUS_DATA_HERE = 'bus_data_here';
-
-export const LINK_LIST = 'link_list';
+export const LINK_LIST = "link_list";
