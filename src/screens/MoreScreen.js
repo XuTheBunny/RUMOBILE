ff76af90ca0209750611713402f2f2fb340bf72f
XuTheBunny <xzhu.fg@gmail.com>
2019-05-03 21:46:08 -0400

    ✨ Add More screen with tabbar for campus

--- src/screens/MoreScreen.js
+++ src/screens/MoreScreen.js
@@ -1,14 +1,90 @@
 import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
-import { View, Text, TouchableWithoutFeedback, Image } from 'react-native';
+import { connect } from 'react-redux';
+import { setCampus } from '../actions';
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native';
+import SegmentedControlTab from 'react-native-segmented-control-tab';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import Header from '../Components/Header';
 import BottomBar from '../Components/BottomBar';
 
-export default class MoreScreen extends Component {
+class MoreScreen extends Component {
+  state = {
+    selectedIndex: 0,
+  };
+
+  componentWillMount() {
+    if (this.props.campus == 'newBrunswick') {
+      this.setState({
+        selectedIndex: 0,
+      });
+    } else {
+      this.setState({
+        selectedIndex: 1,
+      });
+    }
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      selectedIndex: 0,
+    };
+  }
+
+  handleIndexChange = index => {
+    campusIndex = ['newBrunswick', 'newark'];
+    this.props.setCampus(campusIndex[index]);
+    this.setState({
+      selectedIndex: index,
+    });
+  };
+
   render() {
     return (
       <View style={styles.home}>
         <Header text={'More'} />
+        <Text style={styles.titleText}>Rutgers Buses Campus</Text>
+        <View style={styles.titleContainer}>
+          <SegmentedControlTab
+            values={['New Brunswick', 'Newark']}
+            selectedIndex={this.state.selectedIndex}
+            onTabPress={this.handleIndexChange}
+            activeTabStyle={styles.activeTabStyle}
+            tabStyle={styles.tabStyle}
+            tabTextStyle={styles.tabTextStyle}
+          />
+        </View>
+        <ScrollView>
+          <TouchableOpacity>
+            <View style={styles.listContainer}>
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Image style={styles.iconStyle} source={require('../images/More/Classes.png')} />
+                <Text style={styles.listText}>Classes</Text>
+              </View>
+              <EvilIcons name="chevron-right" size={30} color="rgb(138,138,143)" />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View style={styles.listContainer}>
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Image style={styles.iconStyle} source={require('../images/More/Red.png')} />
+                <Text style={styles.listText}>About</Text>
+              </View>
+              <EvilIcons name="chevron-right" size={30} color="rgb(138,138,143)" />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View style={styles.listContainer}>
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Image style={styles.iconStyle} source={require('../images/More/Settings.png')} />
+                <Text style={styles.listText}>Settings</Text>
+              </View>
+              <EvilIcons name="chevron-right" size={30} color="rgb(138,138,143)" />
+            </View>
+          </TouchableOpacity>
+        </ScrollView>
+
         <BottomBar hs={true} bus={true} fs={true} ls={true} mr={false} />
       </View>
     );
@@ -20,4 +96,50 @@ const styles = {
     flex: 1,
     backgroundColor: 'rgb(255, 255, 255)',
   },
+  titleContainer: {
+    paddingHorizontal: 16,
+    marginTop: 10,
+    marginBottom: 25,
+  },
+  titleText: {
+    fontSize: 19,
+    marginLeft: 16,
+  },
+  activeTabStyle: {
+    backgroundColor: '#ed4545',
+  },
+  tabStyle: {
+    borderColor: '#ed4545',
+    height: 30,
+  },
+  tabTextStyle: {
+    color: '#ed4545',
+  },
+  listContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+    borderBottomColor: 'rgb(235,235,235)',
+    borderBottomWidth: 0.5,
+  },
+  iconStyle: {
+    height: 29,
+    width: 29,
+  },
+  listText: {
+    paddingLeft: 15,
+    fontSize: 17,
+  },
 };
+
+const mapStateToProps = state => {
+  return {
+    campus: state.bus.campus,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { setCampus },
+)(MoreScreen);
