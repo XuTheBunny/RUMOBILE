49f355ef26cb3a57a263e71658b1aa83dca6f6ce
XuTheBunny <xzhu.fg@gmail.com>
2019-06-02 22:56:29 -0400

    ✨ Add reduser for class actions with default setting

---
+++ src/reducers/ClassReducer.js
@@ -0,0 +1,36 @@
+import {
+  CLASS,
+  CLASS_DATA_HERE,
+  CLASS_LIST,
+  CLASS_LIST_DATA_HERE,
+  CLASS_SETTING,
+} from '../actions/types';
+
+const INITIAL_STATE = {
+  class_setting: {
+    semester: '92019',
+    campus: 'NB',
+    level: 'UG',
+  },
+  class_list: [],
+  class: [],
+  class_list_data_here: 'no',
+  class_data_here: 'no',
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case CLASS_SETTING:
+      return { ...state, class_setting: action.payload };
+    case CLASS:
+      return { ...state, class: action.payload };
+    case CLASS_DATA_HERE:
+      return { ...state, class_data_here: action.payload };
+    case CLASS_LIST:
+      return { ...state, class_list: action.payload };
+    case CLASS_LIST_DATA_HERE:
+      return { ...state, class_list_data_here: action.payload };
+    default:
+      return state;
+  }
+};
