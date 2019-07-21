7314a1ede2f437fe39d1fbaf1b7b2391f47f5521
XuTheBunny <xzhu.fg@gmail.com>
2019-07-20 20:22:20 -0400

    ♻️ Refactor router structure by separate modals from regular screens

--- Router.js
+++ Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Modal, Overlay, Lightbox } from 'react-native-router-flux';
 import TabIcon from './src/Components/TabIcon';
 import TodayScreen from './src/screens/TodayScreen';
 import FavClassScreen from './src/screens/FavClassScreen';
@@ -20,35 +20,41 @@ import ClassSettingScreen from './src/screens/ClassSettingScreen';
 const RouterComponent = () => {
   return (
     <Router>
-      <Scene key="root" hideNavBar>
-        <Scene key="tabbar" tabs={true} activeTintColor="rgb(237,69,69)">
-          <Scene key="today" title="Today" icon={TabIcon} modal={true}>
-            <Scene key="today_screen" component={TodayScreen} hideNavBar />
-            <Scene key="favClass_screen" component={FavClassScreen} hideNavBar hideTabBar />
-            <Scene key="favBus_screen" component={FavBusScreen} hideNavBar hideTabBar />
-          </Scene>
-          <Scene key="bus" title="Bus" icon={TabIcon}>
-            <Scene key="bus_screen" component={BusScreen} hideNavBar />
-            <Scene key="stop" component={Stop} hideNavBar />
-            <Scene key="route" component={Route} hideNavBar />
-          </Scene>
-          <Scene key="food" title="Food" icon={TabIcon}>
-            <Scene key="food_screen" component={FoodScreen} hideNavBar />
-            <Scene key="food_list" component={FoodList} hideNavBar />
-          </Scene>
-          <Scene key="links" title="Links" icon={TabIcon}>
-            <Scene key="links_screen" component={LinkScreen} hideNavBar />
-          </Scene>
-          <Scene key="more" title="More" icon={TabIcon}>
-            <Scene key="more_screen" component={MoreScreen} hideNavBar />
-            <Scene key="classSetting_screen" component={ClassSettingScreen} hideNavBar hideTabBar />
-            <Scene key="subjects_screen" component={SubjectsScreen} hideNavBar />
-            <Scene key="courses_screen" component={CoursesScreen} hideNavBar />
-            <Scene key="sections_screen" component={SectionsScreen} hideNavBar />
-            <Scene key="section_detail_screen" component={SectionDetailScreen} hideNavBar />
-          </Scene>
-        </Scene>
-      </Scene>
+      <Overlay key="overlay">
+        <Modal key="modal" hideNavBar>
+          <Lightbox key="lightbox">
+            <Scene key="root" hideNavBar>
+              <Scene key="tabbar" tabs={true} activeTintColor="rgb(237,69,69)">
+                <Scene key="today" title="Today" icon={TabIcon}>
+                  <Scene key="today_screen" component={TodayScreen} hideNavBar />
+                </Scene>
+                <Scene key="bus" title="Bus" icon={TabIcon}>
+                  <Scene key="bus_screen" component={BusScreen} hideNavBar />
+                  <Scene key="stop" component={Stop} hideNavBar />
+                  <Scene key="route" component={Route} hideNavBar />
+                </Scene>
+                <Scene key="food" title="Food" icon={TabIcon}>
+                  <Scene key="food_screen" component={FoodScreen} hideNavBar />
+                  <Scene key="food_list" component={FoodList} hideNavBar />
+                </Scene>
+                <Scene key="links" title="Links" icon={TabIcon}>
+                  <Scene key="links_screen" component={LinkScreen} hideNavBar />
+                </Scene>
+                <Scene key="more" title="More" icon={TabIcon}>
+                  <Scene key="more_screen" component={MoreScreen} hideNavBar />
+                  <Scene key="subjects_screen" component={SubjectsScreen} hideNavBar />
+                  <Scene key="courses_screen" component={CoursesScreen} hideNavBar />
+                  <Scene key="sections_screen" component={SectionsScreen} hideNavBar />
+                  <Scene key="section_detail_screen" component={SectionDetailScreen} hideNavBar />
+                </Scene>
+              </Scene>
+            </Scene>
+          </Lightbox>
+          <Scene key="favClass_screen" component={FavClassScreen} hideNavBar />
+          <Scene key="favBus_screen" component={FavBusScreen} hideNavBar />
+          <Scene key="classSetting_screen" component={ClassSettingScreen} hideNavBar />
+        </Modal>
+      </Overlay>
     </Router>
   );
 };
