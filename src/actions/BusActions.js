7ca6fb782b91b08a1e45314a1a69706e5fcad598
XuTheBunny <xzhu.fg@gmail.com>
2019-07-28 01:26:59 -0400

    ✨ Dispatch empty stage payload when getBusStops action enconter error

--- src/actions/BusActions.js
+++ src/actions/BusActions.js
@@ -87,17 +87,25 @@ export const getBusStops = action => {
         Accept: 'application/json',
         'X-Mashape-Key': 'Pcl9MfLNF0mshcAni8CgyFuxVXTap1NA0RxjsnoxN4439f9hBq',
       },
-    }).then(response => {
-      if (response.data.data[agency_id]) {
-        data = response.data.data[agency_id];
-        data.forEach(function(element) {
-          if (!routes_with_bus.includes(element.route_id) && element.arrival_estimates.length > 0) {
-            routes_with_bus.push(element.route_id);
-          }
-        });
-      }
-      resolve(routes_with_bus);
-    });
+    })
+      .then(response => {
+        if (response.data.data[agency_id]) {
+          data = response.data.data[agency_id];
+          data.forEach(function(element) {
+            if (
+              !routes_with_bus.includes(element.route_id) &&
+              element.arrival_estimates.length > 0
+            ) {
+              routes_with_bus.push(element.route_id);
+            }
+          });
+        }
+        resolve(routes_with_bus);
+      })
+      .catch(function(error) {
+        console.log(error);
+        resolve([]);
+      });
   });
 
   var getActiveRoutes = new Promise((resolve, reject) => {
@@ -109,33 +117,38 @@ export const getBusStops = action => {
           Accept: 'application/json',
           'X-Mashape-Key': 'Pcl9MfLNF0mshcAni8CgyFuxVXTap1NA0RxjsnoxN4439f9hBq',
         },
-      }).then(response => {
-        raw_data = response.data.data[agency_id];
-        raw_data.forEach(function(element) {
-          rid = element.route_id;
-          r = {
-            rid: element.route_id,
-            rname: element.long_name,
-            stops: [],
-            isActive: false,
-          };
-          element.stops.forEach(function(e) {
-            s = { sid: e, sname: null, distance: null };
-            r.stops.push(s);
+      })
+        .then(response => {
+          raw_data = response.data.data[agency_id];
+          raw_data.forEach(function(element) {
+            rid = element.route_id;
+            r = {
+              rid: element.route_id,
+              rname: element.long_name,
+              stops: [],
+              isActive: false,
+            };
+            element.stops.forEach(function(e) {
+              s = { sid: e, sname: null, distance: null };
+              r.stops.push(s);
+            });
+            if (element.is_active && values[0].includes(element.route_id)) {
+              r.isActive = true;
+              routes_active.push(r);
+            } else {
+              routes_inactive.push(r);
+            }
+            route_name[element.route_id] = element.long_name;
+            if (!Object.keys(bus_info_collect).includes('r' + element.route_id)) {
+              bus_info_collect['r' + element.route_id] = { rname: element.long_name };
+            }
           });
-          if (element.is_active && values[0].includes(element.route_id)) {
-            r.isActive = true;
-            routes_active.push(r);
-          } else {
-            routes_inactive.push(r);
-          }
-          route_name[element.route_id] = element.long_name;
-          if (!Object.keys(bus_info_collect).includes('r' + element.route_id)) {
-            bus_info_collect['r' + element.route_id] = { rname: element.long_name };
-          }
+          resolve(route_name);
+        })
+        .catch(function(error) {
+          console.log(error);
+          resolve({});
         });
-        resolve(route_name);
-      });
     });
   });
 
@@ -148,80 +161,89 @@ export const getBusStops = action => {
           Accept: 'application/json',
           'X-Mashape-Key': 'Pcl9MfLNF0mshcAni8CgyFuxVXTap1NA0RxjsnoxN4439f9hBq',
         },
-      }).then(response => {
-        data = response.data.data;
-        data.forEach(function(element) {
-          stop_name[element.stop_id] = element.name;
-          s = {};
-          routes = [];
-          s.sid = element.stop_id;
-          s.sname = element.name;
-          element.routes.forEach(function(e) {
-            r = {
-              rid: e,
-              rname: route_name[e],
-              isActive: routes_with_bus.includes(e),
-            };
-            routes.push(r);
-          });
-          s.routes = routes;
-          if (user_location.lat == 'no') {
-            distance = '--';
-          } else {
-            distance = geodist(user_location, element.location, {
-              exact: true,
-              unit: 'miles',
+      })
+        .then(response => {
+          data = response.data.data;
+          data.forEach(function(element) {
+            stop_name[element.stop_id] = element.name;
+            s = {};
+            routes = [];
+            s.sid = element.stop_id;
+            s.sname = element.name;
+            element.routes.forEach(function(e) {
+              r = {
+                rid: e,
+                rname: route_name[e],
+                isActive: routes_with_bus.includes(e),
+              };
+              routes.push(r);
             });
+            s.routes = routes;
+            if (user_location.lat == 'no') {
+              distance = '--';
+            } else {
+              distance = geodist(user_location, element.location, {
+                exact: true,
+                unit: 'miles',
+              });
+            }
+            s.distance = distance == '--' ? '--' : distance.toFixed(2);
+            all_stops.push(s);
+            if (!Object.keys(bus_info_collect).includes('s' + element.stop_id)) {
+              bus_info_collect['s' + element.stop_id] = {
+                sname: element.name,
+                distance: distance == '--' ? '--' : distance.toFixed(1),
+              };
+            }
+          });
+          routes_active.forEach(function(element) {
+            element.stops.forEach(function(e) {
+              e.sname = stop_name[e.sid];
+              e.distance = all_stops.find(obj => obj.sid == e.sid).distance;
+            });
+          });
+          routes_inactive.forEach(function(element) {
+            element.stops.forEach(function(e) {
+              e.sname = stop_name[e.sid];
+              e.distance = all_stops.find(obj => obj.sid == e.sid).distance;
+            });
+          });
+          dispatch({ type: BUS_INFO, payload: bus_info_collect });
+          dispatch({ type: ACTIVEROUTES, payload: routes_active });
+          dispatch({ type: INACTIVEROUTES, payload: routes_inactive });
+          if (all_stops[0].distance == '--') {
+            dispatch({
+              type: NEARBYBUS,
+              payload: [],
+            });
+            dispatch({
+              type: ALLBUS,
+              payload: all_stops.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0)),
+            });
+            dispatch({ type: LOCATION_SHARING, payload: false });
+          } else {
+            dispatch({
+              type: NEARBYBUS,
+              payload: all_stops
+                .sort((a, b) => (a.distance > b.distance ? 1 : b.distance > a.distance ? -1 : 0))
+                .slice(0, nearby_count),
+            });
+            dispatch({
+              type: ALLBUS,
+              payload: all_stops.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0)),
+            });
+            dispatch({ type: LOCATION_SHARING, payload: true });
           }
-          s.distance = distance == '--' ? '--' : distance.toFixed(2);
-          all_stops.push(s);
-          if (!Object.keys(bus_info_collect).includes('s' + element.stop_id)) {
-            bus_info_collect['s' + element.stop_id] = {
-              sname: element.name,
-              distance: distance == '--' ? '--' : distance.toFixed(1),
-            };
-          }
+          dispatch({ type: BUS_DATA_HERE, payload: 'here' });
+        })
+        .catch(function(error) {
+          console.log(error);
+          dispatch({ type: ACTIVEROUTES, payload: [] });
+          dispatch({ type: INACTIVEROUTES, payload: [] });
+          dispatch({ type: NEARBYBUS, payload: [] });
+          dispatch({ type: ALLBUS, payload: [] });
+          dispatch({ type: BUS_DATA_HERE, payload: 'no' });
         });
-        routes_active.forEach(function(element) {
-          element.stops.forEach(function(e) {
-            e.sname = stop_name[e.sid];
-            e.distance = all_stops.find(obj => obj.sid == e.sid).distance;
-          });
-        });
-        routes_inactive.forEach(function(element) {
-          element.stops.forEach(function(e) {
-            e.sname = stop_name[e.sid];
-            e.distance = all_stops.find(obj => obj.sid == e.sid).distance;
-          });
-        });
-        dispatch({ type: BUS_INFO, payload: bus_info_collect });
-        dispatch({ type: ACTIVEROUTES, payload: routes_active });
-        dispatch({ type: INACTIVEROUTES, payload: routes_inactive });
-        if (all_stops[0].distance == '--') {
-          dispatch({
-            type: NEARBYBUS,
-            payload: [],
-          });
-          dispatch({
-            type: ALLBUS,
-            payload: all_stops.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0)),
-          });
-          dispatch({ type: LOCATION_SHARING, payload: false });
-        } else {
-          dispatch({
-            type: NEARBYBUS,
-            payload: all_stops
-              .sort((a, b) => (a.distance > b.distance ? 1 : b.distance > a.distance ? -1 : 0))
-              .slice(0, nearby_count),
-          });
-          dispatch({
-            type: ALLBUS,
-            payload: all_stops.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0)),
-          });
-          dispatch({ type: LOCATION_SHARING, payload: true });
-        }
-        dispatch({ type: BUS_DATA_HERE, payload: 'here' });
-      });
     });
   };
 };
