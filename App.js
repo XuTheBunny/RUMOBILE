8148eaa5b1bf2dd8642de0544d300ccf68314bc2
XuTheBunny <xzhu.fg@gmail.com>
2019-07-29 20:03:24 -0400

    🔥 Remove obsolete firebase config

--- App.js
+++ App.js
@@ -1,28 +1,9 @@
 import React, { Component } from 'react';
-import {
-  API_KEY,
-  AUTH_DOMAIN,
-  DATABASE_URL,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGE_SENDER_ID,
-} from './env.json';
 import { Provider } from 'react-redux';
-import firebase from 'firebase';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import Router from './Router';
 import reducers from './src/reducers';
-
-const config = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  databaseURL: DATABASE_URL,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGE_SENDER_ID,
-};
-firebase.initializeApp(config);
 
 export default class App extends Component {
   render() {
